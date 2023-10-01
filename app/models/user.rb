183185f0743bf8f6ee9f514dbd9d6baf4ec3e976
EC2 Default User <ec2-user@ip-172-31-29-217.ap-northeast-1.compute.internal>
2023-10-01 23:22:56 +0000

    updata

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_many :books, dependent: :destroy
+  has_many :books, dependent: :destroy
+  
+  
+  validates :name, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: { maximum: 50 }
+
+  after_create :send_email_on_create
+
+  def send_email_on_create
+    NotificationMailer.complete_mail(self).deliver_now
+  end
 end
